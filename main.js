f152817742b349d1419ebe6cb8b2e5e17676a8e6
majidali321 <159675272+majidali321@users.noreply.github.com>
2024-02-22 20:08:47 +0500

    2nd task of 45 questions

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// console.log("Hello world")
+// console.log("majid ali")
+let Name = "Majid ali";
+console.log("hello", (Name), "would you like to  learn some TypeScript today.");
---
+++ main.ts
@@ -0,0 +1,4 @@
+// console.log("Hello world")
+// console.log("majid ali")
+let Name:string = "Majid ali";
+console.log("hello",(Name) , "would you like to  learn some TypeScript today.");
